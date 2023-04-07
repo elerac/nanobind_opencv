c97a0e18903aa4e0f0fbfd2318c4b01b1647818c
Ryota MAEDA <maenosuke079@gmail.com>
2023-04-07 20:00:51 +0900

    Update nanobind_opencv_example.cpp

--- src/nanobind_opencv_example.cpp
+++ src/nanobind_opencv_example.cpp
@@ -17,55 +17,24 @@ public:
     ViewData(const cv::Mat3b &mat, const cv::Matx33f &K, const cv::Matx33f &R, const cv::Vec3f &t) : mat(mat), K(K), R(R), t(t) {}
     ViewData(const cv::Mat3b &mat) : mat(mat), K(cv::Matx33f::eye()), R(cv::Matx33f::eye()), t(cv::Vec3f::all(0)) {}
 
-    cv::Matx34f get_camera_matrix() const
+    void set_focal_length(float focal_length)
     {
-        // Copy rotation matrix and translation vector to extrinsic matrix
-        cv::Matx44f extrinsic = cv::Matx44f::eye();
-        extrinsic(0, 0) = R(0, 0);
-        extrinsic(0, 1) = R(0, 1);
-        extrinsic(0, 2) = R(0, 2);
-        extrinsic(1, 0) = R(1, 0);
-        extrinsic(1, 1) = R(1, 1);
-        extrinsic(1, 2) = R(1, 2);
-        extrinsic(2, 0) = R(2, 0);
-        extrinsic(2, 1) = R(2, 1);
-        extrinsic(2, 2) = R(2, 2);
-        extrinsic(0, 3) = t(0);
-        extrinsic(1, 3) = t(1);
-        extrinsic(2, 3) = t(2);
-
-        // Copy intrinsic matrix
-        cv::Matx33f intrinsic = K;
-
-        // Dot product of intrinsic and extrinsic
-        cv::Matx34f camera_matrix = cv::Matx34f::zeros();
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                for (int k = 0; k < 4; k++)
-                {
-                    camera_matrix(i, j) += intrinsic(i, k) * extrinsic(k, j);
-                }
-            }
-        }
-
-        return camera_matrix;
+        K(0, 0) = focal_length;
+        K(1, 1) = focal_length;
     }
 };
 
 template <typename _Tp>
-cv::Mat_<_Tp> inspect(const cv::Mat_<_Tp> mat)
+void inspect(const cv::Mat_<_Tp> mat)
 {
     std::cout << "[C++] Inspect cv::Mat_<_Tp>" << std::endl;
     std::cout << "        rows: " << mat.rows << std::endl;
     std::cout << "        cols: " << mat.cols << std::endl;
     std::cout << "        channels: " << mat.channels() << std::endl;
     std::cout << "        type: " << cv::typeToString(mat.type()) << std::endl;
-    return mat;
 }
 
-NB_MODULE(example_ext, m)
+NB_MODULE(_nanobind_opencv_example_impl, m)
 {
     m.def("inspect", &inspect<cv::Vec3b>, nb::arg("mat").noconvert());
     m.def("inspect", &inspect<cv::Vec3f>, nb::arg("mat").noconvert());
@@ -75,9 +44,9 @@ NB_MODULE(example_ext, m)
     nb::class_<ViewData>(m, "ViewData")
         .def(nb::init<const cv::Mat3b &, const cv::Matx33f &, const cv::Matx33f &, const cv::Vec3f &>(), "mat"_a.noconvert(), "K"_a, "R"_a, "t"_a)
         .def(nb::init<const cv::Mat3b &>(), "mat"_a)
+        .def("set_focal_length", &ViewData::set_focal_length, "focal_length"_a)
         .def_rw("mat", &ViewData::mat)
         .def_rw("K", &ViewData::K)
         .def_rw("R", &ViewData::R)
-        .def_rw("t", &ViewData::t)
-        .def("get_camera_matrix", &ViewData::get_camera_matrix);
+        .def_rw("t", &ViewData::t);
 }
